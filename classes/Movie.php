04165909bab615e12093554a11105d90be93bc90
GLuca27 <gianluca.sciglitano1@gmail.com>
2023-01-16 17:49:24 +0100

    finito

--- classes/Movie.php
+++ classes/Movie.php
@@ -6,8 +6,17 @@ class Movie {
     public $year;
     public $duration;
 
-    public function __construct ($title, $genre, $duration)
+    public function __construct (string $title, string $genre, int $duration)
     {
-        
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->duration = $duration;
+    }
+
+    //metodo
+    public function getInfo()
+    {
+        return "Titolo: {$this->title}, il film dura: {$this->duration}.";
     }
 }
+
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once __DIR__ . "/classes/Movie.php";
 
-$movie = new Movie("The Equalizer - Il vendicatore", "Azione", "2h 12m");
+$movie = new Movie("The Equalizer - Il vendicatore", "Azione", 132);
 
-var_dump($movie);
+echo $movie->getInfo();
